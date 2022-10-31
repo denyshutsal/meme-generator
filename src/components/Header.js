ac8030db335447bce595b5bd897903f6374a775e
Denys Hutsal <denyshutsal@gmail.com>
2022-10-31 14:55:04 +0100

    feat: add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.scss';
+import Logo from './Logo';
+
+export default function Header() {
+  return (
+    <header className="app-header">
+      <Logo />
+    </header>
+  );
+}
